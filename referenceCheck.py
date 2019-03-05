f385e324d56842bb4c8bc67874c39f09fe8fc521
Todd Pihl <todd.pihl@gmail.com>
2019-03-05 14:52:07 -0500

    referenceCheck update

--- referenceCheck.py
+++ referenceCheck.py
@@ -7,7 +7,7 @@ import re
 import pprint
 
 def getMD5(headerfile, md5list, verbose):
-  #Returns a list of all unique md5 values in a samtools header file
+  #Returns a dictionary  of all unique md5 values in a samtools header file and the files they are found in
   md5list = []
   headerfile = headerfile.rstrip()
   if verbose:
@@ -23,9 +23,6 @@ def getMD5(headerfile, md5list, verbose):
                 md5list[md5] = md5list[md5].append(headerfile)
             else:
                 md5list[md5] = [headerfile]
-
-            #if md5 not in md5list:
-              #md5list.append(md5)
   return md5list
 
 def main (args):
@@ -37,11 +34,11 @@ def main (args):
   #Read the list of header files
   headerfiles = open(args.inputfile,'r')
   md5list = {}
+  #For each headerfile, get a list of overall unique md5 values
   for headerfile in headerfiles:
     logfile.write("Checking file " + headerfile)
     md5list = getMD5(headerfile, md5list, args.verbose)
-    #if args.verbose:
-    #  pprint.pprint(md5list)
+  #Check each md5 against the ENA database and log only failures
   for (md5, filelist) in md5list.items():
     try:
       r = requests.get(baseurl + md5 + "/metadata")
@@ -51,7 +48,6 @@ def main (args):
         pprint.pprint(r.json())
       if r.json()['metadata']['md5'] is None:
         logfile.writ(("MD5:\t%s\tMD5 Error:\tNot found\tFiles:\t%s\t") % (md5, ' '.join(filelist)))
-        #logfile.write(("File\t%s\tMD5:\t%s\tMD5 value not found\n") % (headerfile, md5))
     except requests.exceptions.Timeout as exception:
       logfile.write(("MD5:\t%s\tTimeout error:\t%s\tFiles:\t%s\n") % (md5,exception, ' '.join(filelist)))
     except requests.exceptions.HTTPError as exception:
